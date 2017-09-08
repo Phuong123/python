c95a70fc2e4c160ad9feb2874865ba9be27f967c
phuong <phthph@gmail.com>
2017-09-08 14:28:33 +0200

    basic

--- basic/pythonDictionary.py
+++ basic/pythonDictionary.py
@@ -11,3 +11,21 @@ my_dict['address'] = 'Downtown'
 
 # Output: {'address': 'Downtown', 'age': 27, 'name': 'Jack'}
 print(my_dict)
+
+squares = {x: x*x for x in range(6)}
+
+# Output: {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+print(squares)
+
+
+squares = {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
+
+# Output: True
+print(1 in squares)
+
+# Output: True
+print(2 not in squares)
+
+# membership tests for key only not value
+# Output: False
+print(49 in squares)
